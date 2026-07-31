2bcf5a03dbfb88519ab01cce70ba6474305bc0fb
Serge <sajghaa@gmail.com>
2026-07-31 16:49:25 +0200

    Adds LazySingleton

--- 01-Creational/Singleton/src/LazySingleton.java
+++ 01-Creational/Singleton/src/LazySingleton.java
@@ -1,3 +1,44 @@
 public class LazySingleton {
     
+
+    private static volatile LazySingleton instance;
+
+    private LazySingleton(){
+        System.out.println("LazySingleton instance created!");
+    }
+
+    public static LazySingleton getInstance(){
+        if (instance == null){
+            synchronized (LazySingleton.class){
+                if (instance == null){
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+    public void showMessage(){
+        System.out.println("Hello from LazySingleton! Hash:" + this.hashCode());
+    }
+
+    public static void main(String[]args){
+        Runnable task = () ->{
+            LazySingleton obj = LazySingleton.getInstance();
+            System.out.println(Thread.currentThread().getName() + " -> " + obj.hashCode());
+        };
+
+        Thread t1 = new Thread(task, "Thread-A");
+        Thread t2 = new Thread(task, "Thread-B");
+        Thread t3 = new Thread(task, "Thread-C");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try{
+            Thread.sleep(500);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 }
