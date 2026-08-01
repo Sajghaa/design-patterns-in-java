452b8683d40e5e3ad0d926fb76e4049518d6629b
Serge <sajghaa@gmail.com>
2026-08-01 20:04:27 +0200

    update Circle.java

--- 01-Creational/Factory/src/shapes/Circle.java
+++ 01-Creational/Factory/src/shapes/Circle.java
@@ -1,5 +1,19 @@
 package shapes;
 
-public class Circle {
-    
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius
+    }
+
+    @Override
+    public void draw(){
+        System.out.println("Drawing a Circle with radius" + radius);
+    }
+
+    @Override
+    public double area(){
+        return Math.PI * radius *radius;
+    }
 }
