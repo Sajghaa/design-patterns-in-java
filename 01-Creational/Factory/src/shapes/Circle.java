195f57934544e6aac1c0385ea953680c03637382
Serge <sajghaa@gmail.com>
2026-08-01 20:09:59 +0200

    Small fixes on Circle.java

--- 01-Creational/Factory/src/shapes/Circle.java
+++ 01-Creational/Factory/src/shapes/Circle.java
@@ -4,7 +4,7 @@ public class Circle implements Shape {
     private double radius;
 
     public Circle(double radius){
-        this.radius = radius
+        this.radius = radius;
     }
 
     @Override
