85287c38c0c1acb7df5ba7919693e70d6837c4b5
Serge <sajghaa@gmail.com>
2026-08-04 22:45:18 +0200

    updats Decorator(DeliveryFeeDecorator)

--- 02-Structural/Decorator/Exercises/AccountingSystem/DeliveryFeeDecorator.java
+++ 02-Structural/Decorator/Exercises/AccountingSystem/DeliveryFeeDecorator.java
@@ -1,5 +1,12 @@
 package Decorator.Exercises.AccountingSystem;
 
-public class DeliveryFeeDecorator {
+public class DeliveryFeeDecorator extends InvoiceDecorator{
+    public DeliveryFeeDecorator(Invoice invoice){
+        super(invoice);
+    }
+    @Override
+    public double getAmount(){
+        return super.getAmount() + 2000;
+    }
     
 }
