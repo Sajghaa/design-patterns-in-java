b1e385670102c5e82b2c00e4d40e34ce8fcebd7a
Serge <sajghaa@gmail.com>
2026-08-04 22:44:26 +0200

    Adds our Plain version/Default Invoice

--- 02-Structural/Decorator/Exercises/AccountingSystem/BasicInvoice.java
+++ 02-Structural/Decorator/Exercises/AccountingSystem/BasicInvoice.java
@@ -1,5 +1,15 @@
 package Decorator.Exercises.AccountingSystem;
 
-public class BasicInvoice {
-    
+public class BasicInvoice implements Invoice{
+
+    private double subtotal;
+
+    public BasicInvoice(double subtotal){
+        this.subtotal = subtotal;
+    }
+
+    @Override
+    public double getAmount(){
+        return subtotal;
+    }
 }
