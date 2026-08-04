d8fe21412290fb91b717edc0a465b30ffc3c6157
Serge <sajghaa@gmail.com>
2026-08-04 22:46:15 +0200

    Adds InvoiceDecoratorDemo/main class

--- 02-Structural/Decorator/Exercises/AccountingSystem/InvoiceDecoratorDemo.java
+++ 02-Structural/Decorator/Exercises/AccountingSystem/InvoiceDecoratorDemo.java
@@ -1,5 +1,22 @@
 package Decorator.Exercises.AccountingSystem;
 
 public class InvoiceDecoratorDemo {
-    
+    public static void main(String[]args){
+        Invoice basic = new BasicInvoice(500);
+        System.out.println("Basic Invoice: " + basic.getAmount());
+
+        Invoice withDelivery = new DeliveryFeeDecorator(basic);
+        System.out.println("With delivery: " + withDelivery.getAmount());
+
+        Invoice withBoth = new InsuranceFeeDecorator(withDelivery);
+        System.out.println("With delivery + insurance: " + withBoth.getAmount());
+
+        Invoice full = new InsuranceFeeDecorator(
+            new DeliveryFeeDecorator(
+                new BasicInvoice(500)
+            )
+        );
+        System.out.println("Final amount: " + full.getAmount());
+    }
 }
+
