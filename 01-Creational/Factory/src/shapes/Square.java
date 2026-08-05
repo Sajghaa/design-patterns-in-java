2af49cdf0d1c6875b10a87c70cc488512b25d796
Serge <sajghaa@gmail.com>
2026-08-05 22:39:19 +0200

    updates File

--- 01-Creational/Factory/src/shapes/Square.java
+++ 01-Creational/Factory/src/shapes/Square.java
@@ -1,5 +1,20 @@
 package shapes;
 
-public class Square {
+public class Square implements Shape {
     
+    private double side;
+
+    public Square(double side){
+        this.side = side;
+    }
+
+    @Override
+    public void draw(){
+        System.out.println("Drawing a Square with side " + side);
+    }
+
+    @Override
+    public double area(){
+        return side * side;
+    }
 }
