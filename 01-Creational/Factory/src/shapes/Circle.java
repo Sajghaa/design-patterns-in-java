72ad8e1a6caba30e9237859bc3999587220eb4fd
Serge <sajghaa@gmail.com>
2026-08-09 21:19:51 +0200

    updates file(Circle.java)

--- 01-Creational/Factory/src/shapes/Circle.java
+++ 01-Creational/Factory/src/shapes/Circle.java
@@ -9,7 +9,7 @@ public class Circle implements Shape {
 
     @Override
     public void draw(){
-        System.out.println("Drawing a Circle with radius" + radius);
+        System.out.println("Drawing a Circle (r=" + radius + ")");
     }
 
     @Override
