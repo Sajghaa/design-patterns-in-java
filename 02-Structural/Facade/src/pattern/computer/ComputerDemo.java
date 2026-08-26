2ad6b6e97c0de92765378b5fc6fc31ce82864962
Serge <sajghaa@gmail.com>
2026-08-26 21:54:29 +0200

    update ComputerDemo.java

--- 02-Structural/Facade/src/pattern/computer/ComputerDemo.java
+++ 02-Structural/Facade/src/pattern/computer/ComputerDemo.java
@@ -1,5 +1,10 @@
 package pattern.computer;
 
 public class ComputerDemo {
-    
+    CPU cpu = new CPU();
+    Memory memory = new Memory();
+    HardDrive hardDrive = new HardDrive();
+
+    ComputerFacade computer = new ComputerFacade(cpu, memory, hardDrive);
+    computer.start();
 }
