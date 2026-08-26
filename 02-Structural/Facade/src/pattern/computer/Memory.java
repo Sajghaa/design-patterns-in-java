5ea370b242c10905cdb60e821045f1a438c90fe6
Serge <sajghaa@gmail.com>
2026-08-26 21:55:44 +0200

    update Memory.java

--- 02-Structural/Facade/src/pattern/computer/Memory.java
+++ 02-Structural/Facade/src/pattern/computer/Memory.java
@@ -1,5 +1,7 @@
 package pattern.computer;
 
 public class Memory {
-    
+    public void load(long position, byte[] data){
+        System.out.println("Memory: Loading data at " + position + "(Size: " + data.length +")");
+     }
 }
