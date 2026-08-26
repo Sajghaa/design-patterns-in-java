47db7aa771278b4071b3eedab1328cfb99b91770
Serge <sajghaa@gmail.com>
2026-08-26 21:41:45 +0200

    Add SmartHomeDemo.java

--- 02-Structural/Facade/src/pattern/smarthome/SmartHomeDemo.java
+++ 02-Structural/Facade/src/pattern/smarthome/SmartHomeDemo.java
@@ -1,5 +1,18 @@
 package pattern.smarthome;
 
 public class SmartHomeDemo {
-    
+    public static void main(String[] args) {
+        
+        Light light = new Light();
+        Thermostat thermostat = new Thermostat();
+        SecuritySystem security = new SecuritySystem();
+
+        SmartHomeFacade facade = new SmartHomeFacade(light, thermostat, security);
+
+        facade.arriveHome();
+        System.out.println();
+        facade.leaveHome();
+
+        System.out.println("\n Added DoorLock to Facade? Client doesn't care! It just calls leaveHome().");
+    }
 }
