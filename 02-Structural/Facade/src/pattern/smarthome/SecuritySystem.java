f2aa193e2d2040869bf397fbcba41597b6346a86
Serge <sajghaa@gmail.com>
2026-08-26 21:39:34 +0200

    Add SecuritySystem

--- 02-Structural/Facade/src/pattern/smarthome/SecuritySystem.java
+++ 02-Structural/Facade/src/pattern/smarthome/SecuritySystem.java
@@ -2,4 +2,6 @@ package pattern.smarthome;
 
 public class SecuritySystem {
     
+    public void arm() { System.out.println("Security system ARMED"); }
+    public void disarm() { System.out.println("Security system DISARMED"); }
 }
