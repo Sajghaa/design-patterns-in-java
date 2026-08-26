1d76d4df6489267bda9877a203acaf40f1209bd9
Serge <sajghaa@gmail.com>
2026-08-26 21:55:02 +0200

    update ComputerFacade.java

--- 02-Structural/Facade/src/pattern/computer/ComputerFacade.java
+++ 02-Structural/Facade/src/pattern/computer/ComputerFacade.java
@@ -1,5 +1,22 @@
 package pattern.computer;
 
 public class ComputerFacade {
-    
+    private CPU cpu;
+    private Memory memory;
+    private HardDrive hardDrive;
+
+    public ComputerFacade(CPU cpu, Memory memory, HardDrive hardDrive) {
+        this.cpu = cpu;
+        this.memory = memory;
+        this.hardDrive = hardDrive;
+    }
+
+    public void start() {
+        System.out.println("=== Starting computer (via Facade) ===");
+        cpu.freeze();
+        memory.load(0,hardDrive.read(0, 1024));
+        cpu.jump(0);
+        cpu.execute();
+        System.out.println("Computer ready to use!");
+    }
 }
