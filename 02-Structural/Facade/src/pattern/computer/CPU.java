7df7f75a48735eef8334572fe19b55f466034afc
Serge <sajghaa@gmail.com>
2026-08-26 21:54:11 +0200

    Add CPU.jav

--- 02-Structural/Facade/src/pattern/computer/CPU.java
+++ 02-Structural/Facade/src/pattern/computer/CPU.java
@@ -1,5 +1,7 @@
 package pattern.computer;
 
 public class CPU {
-    
+    public void freeze() { System.out.println("CPU: Freezing..."); }
+    public void jump(long position) { System.out.println("CPU: Jumping to " + position); }
+    public void execute() { System.out.println("CPU: Executing instructions"); }
 }
