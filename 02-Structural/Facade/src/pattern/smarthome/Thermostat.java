73e298510db2dfb63f8f06396faadce43e44e6ce
Serge <sajghaa@gmail.com>
2026-08-26 21:39:58 +0200

    Add Thermostat.java

--- 02-Structural/Facade/src/pattern/smarthome/Thermostat.java
+++ 02-Structural/Facade/src/pattern/smarthome/Thermostat.java
@@ -1,5 +1,7 @@
 package pattern.smarthome;
 
 public class Thermostat {
-    
+    public void setTemperature(int temp) {
+        System.out.println("Thermostat set to " + temp + "degress");
+    }
 }
