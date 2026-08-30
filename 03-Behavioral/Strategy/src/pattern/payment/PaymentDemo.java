0c6f5267477b81c8f24ee398bc2cfff59f244d6c
Serge <sajghaa@gmail.com>
2026-08-30 21:33:50 +0200

    Update PaymentDemo.java

--- 03-Behavioral/Strategy/src/pattern/payment/PaymentDemo.java
+++ 03-Behavioral/Strategy/src/pattern/payment/PaymentDemo.java
@@ -1,5 +1,16 @@
 package pattern.payment;
 
 public class PaymentDemo {
-    
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+
+        context.setStrategy(new CreditCardStrategy("1234-5678-9012-3456", "123"));
+        context.executePayment(250.00);
+
+        context.setStrategy(new PayPalStrategy("predator@hunting.com"));
+        context.executePayment(150.00);
+
+        context.setStrategy(new CryptoStrategy("0xABC123DEF456"));
+        context.executePayment(500.00);
+    }
 }
