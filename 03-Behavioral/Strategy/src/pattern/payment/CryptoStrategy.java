f88f40d81e7a18176a19e3abe5cedf861a9a464d
Serge <sajghaa@gmail.com>
2026-08-30 21:31:29 +0200

    Update CryptoStrategy.java

--- 03-Behavioral/Strategy/src/pattern/payment/CryptoStrategy.java
+++ 03-Behavioral/Strategy/src/pattern/payment/CryptoStrategy.java
@@ -1,5 +1,14 @@
 package pattern.payment;
 
-public class CryptoStrategy {
+public class CryptoStrategy implements PaymentStrategy {
     
+    private String walletAddress;
+
+    public CryptoStrategy(String walletAddress){
+        this.walletAddress = walletAddress;
+    }
+    @Override
+    public void pay(double amount){
+        System.out.println(" Paid " + amount + " using crypto (wallet: " + walletAddress.substring(0, 6) + "...)");
+    }
 }
