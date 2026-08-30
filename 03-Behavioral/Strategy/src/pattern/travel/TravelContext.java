efa7df795733cc01c343b3c4f1d9b14713040784
Serge <sajghaa@gmail.com>
2026-08-30 21:13:53 +0200

    Update TravelContext.java

--- 03-Behavioral/Strategy/src/pattern/travel/TravelContext.java
+++ 03-Behavioral/Strategy/src/pattern/travel/TravelContext.java
@@ -1,5 +1,18 @@
 package pattern.travel;
 
 public class TravelContext {
-    
+ 
+    private FareStrategy strategy;
+
+    public void setFareStrategy(FareStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void printFare() {
+        if (strategy == null) {
+            System.out.println(" No fare strategy set!");
+        } else {
+            System.out.println("Fare: " + strategy.calculateFare() + " RWF ");
+        }
+    }
 }
