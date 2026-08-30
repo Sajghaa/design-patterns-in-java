465a101d1f3271a3107a40a9a53176e17ca4ada4
Serge <sajghaa@gmail.com>
2026-08-30 21:31:57 +0200

    Update PayPalStrategy.java

--- 03-Behavioral/Strategy/src/pattern/payment/PayPalStrategy.java
+++ 03-Behavioral/Strategy/src/pattern/payment/PayPalStrategy.java
@@ -1,5 +1,15 @@
 package pattern.payment;
 
-public class PayPalStrategy {
+public class PayPalStrategy implements PaymentStrategy {
     
+    private String email;
+
+    public PayPalStrategy(String email){
+        this.email = email;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println(" Paid " + amount + " using Paypal (" + email + ")");
+    }
 }
