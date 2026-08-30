17866ec31abb88c9f0b915a04729572527320112
Serge <sajghaa@gmail.com>
2026-08-30 21:32:29 +0200

    Update PaymentContext.java

--- 03-Behavioral/Strategy/src/pattern/payment/PaymentContext.java
+++ 03-Behavioral/Strategy/src/pattern/payment/PaymentContext.java
@@ -1,5 +1,18 @@
 package pattern.payment;
 
 public class PaymentContext {
-    
+ 
+    private PaymentStrategy strategy;
+
+    public void setStrategy(PaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void executePayment(double amount) {
+        if (strategy == null) {
+            System.out.println("Payment strategy not set");
+        } else {
+            strategy.pay(amount);
+        }
+    }
 }
