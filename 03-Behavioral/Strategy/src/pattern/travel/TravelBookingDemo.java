5e34eaea1ea8ddda0fbd73feca21aa269520b03c
Serge <sajghaa@gmail.com>
2026-08-30 21:13:27 +0200

    Update TravelBookingDemo.java

--- 03-Behavioral/Strategy/src/pattern/travel/TravelBookingDemo.java
+++ 03-Behavioral/Strategy/src/pattern/travel/TravelBookingDemo.java
@@ -1,5 +1,18 @@
 package pattern.travel;
 
 public class TravelBookingDemo {
-    
+    public static void main(String[] args) {
+        TravelContext context = new TravelContext();
+
+        context.setFareStrategy(new EconomyFare());
+        context.printFare();
+
+        context.setFareStrategy(new BusinessFare());
+        context.printFare();
+
+        context.setFareStrategy(new FirstClassFare());
+        context.printFare();
+
+        System.out.println("\n OCP Achieved! Adding Student requires ZERO changes to existing strategy classes.");
+    }
 }
