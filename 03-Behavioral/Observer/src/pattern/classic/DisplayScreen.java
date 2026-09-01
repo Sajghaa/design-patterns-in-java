4d2257e7e7ba5ecd1525b8a43b5c816b8ab2e5b5
Serge <sajghaa@gmail.com>
2026-09-01 16:56:00 +0200

    Updated DisplayScreen.java

--- 03-Behavioral/Observer/src/pattern/classic/DisplayScreen.java
+++ 03-Behavioral/Observer/src/pattern/classic/DisplayScreen.java
@@ -1,5 +1,24 @@
 package pattern.classic;
 
-public class DisplayScreen {
+public class DisplayScreen implements Observer {
     
+    private WeatherData weatherData;
+    private double temperature;
+    private double humidity;
+
+    public DisplayScreen(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.attach(this);
+    }
+
+    @Override
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        display();
+    }
+
+    private void display() {
+        System.out.println("Display: Temp=" + temperature + "°C, Humidity=" + humidity + "%");
+    }
 }
