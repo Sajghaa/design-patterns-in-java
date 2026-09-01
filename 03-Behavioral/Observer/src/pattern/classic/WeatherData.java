c363e3cd2c580f06139092274173d288b0ae049c
Serge <sajghaa@gmail.com>
2026-09-01 16:57:02 +0200

    Update WeatherData.java

--- 03-Behavioral/Observer/src/pattern/classic/WeatherData.java
+++ 03-Behavioral/Observer/src/pattern/classic/WeatherData.java
@@ -1,5 +1,25 @@
 package pattern.classic;
 
-public class WeatherData {
+public class WeatherData  extends Subject {
     
+    private double temperature;
+    private double humidity;
+    private double pressure;
+
+
+    public void setMeasurements(double temperature, double humidity, double pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    private void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public double getTemperature() { return temperature; }
+    public double getHumidity() { return humidity; }
+    public double getPressure() { return pressure; }
+
 }
