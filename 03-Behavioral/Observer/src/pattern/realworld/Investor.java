6d75e15726a7cd52070cefb5d6383b42a527847b
Serge <sajghaa@gmail.com>
2026-09-01 16:58:02 +0200

    Update Investor.java

--- 03-Behavioral/Observer/src/pattern/realworld/Investor.java
+++ 03-Behavioral/Observer/src/pattern/realworld/Investor.java
@@ -2,4 +2,13 @@ package pattern.realworld;
 
 public class Investor {
     
+    private String name;
+
+    public Investor(String name) {
+        this.name = name;
+    }
+
+    public void update(Stock stock) {
+        System.out.println("📈" + name + " notified: " + stock.getSymbol() + " is now $" + stock.getPrice());
+    }
 }
