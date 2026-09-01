591b5a0640d504253284afc34953bac1957133df
Serge <sajghaa@gmail.com>
2026-09-01 16:58:41 +0200

    Update StockMarketDemo.java

--- 03-Behavioral/Observer/src/pattern/realworld/StockMarketDemo.java
+++ 03-Behavioral/Observer/src/pattern/realworld/StockMarketDemo.java
@@ -2,4 +2,26 @@ package pattern.realworld;
 
 public class StockMarketDemo {
     
+    public static void main(String[] args) {
+        Stock apple = new Stock("AAPL", 150.0);
+    Stock google = new Stock("GOOGL", 2800.0);
+
+    Investor elon = new Investor("Elon Must");
+    Investor warren = new Investor("Warren Buffet");
+    Investor cathie = new Investor("Cathie Wood");
+
+    apple.attach(elon);
+    apple.attach(warren);
+    google.attach(cathie);
+    google.attach(warren);
+
+    System.out.println("=== Stock Market Live ===");
+    apple.setPrice(155.0);
+    google.setPrice(2850.0);
+    apple.setPrice(160.0);
+
+    System.out.println("\n Adding investors does NOT affect Stock class!");
+    }
+    
+
 }
