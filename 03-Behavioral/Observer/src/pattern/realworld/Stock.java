c1a4e4a056d957c67439892810d5524cb6833645
Serge <sajghaa@gmail.com>
2026-09-01 16:58:18 +0200

    Update Stock.java

--- 03-Behavioral/Observer/src/pattern/realworld/Stock.java
+++ 03-Behavioral/Observer/src/pattern/realworld/Stock.java
@@ -1,5 +1,40 @@
 package pattern.realworld;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stock {
     
+    private String symbol;
+    private double price;
+    private List<Investor> investors = new ArrayList<>();
+
+    public Stock(String symbol, double price) {
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public void attach(Investor investor) {
+        investors.add(investor);
+    }
+
+    public void detach(Investor investor) {
+        investors.remove(investor);
+    }
+
+    public void setPrice(double price) {
+        if (this.price != price) {
+            this.price = price;
+            notifyInvestors();
+        }
+    }
+
+    private void notifyInvestors() {
+        for (Investor investor: investors) {
+            investor.update(this);
+        }
+    }
+
+    public String getSymbol() { return symbol; }
+    public double getPrice() { return price; }
 }
