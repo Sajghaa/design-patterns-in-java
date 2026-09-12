9c3809e31680405445a45bd9e15cbb0d5ac57e3a
Serge <sajghaa@gmail.com>
2026-09-12 21:43:43 +0200

    Updated StorageClient.java

--- 01-Creational/AbstractFactory/src/main/java/com/designpatterns/abstractfactory/storage/product/StorageClient.java
+++ 01-Creational/AbstractFactory/src/main/java/com/designpatterns/abstractfactory/storage/product/StorageClient.java
@@ -1,5 +1,14 @@
 package com.designpatterns.abstractfactory.storage.product;
 
-public class StorageClient {
+
+public interface StorageClient {
+
+  void upload(String bucket, String key, byte[] data);
+  
+  byte[] download(String bucket, String key);
+
+  void delete(String bucket, String Key);
+
+  String providerName();
     
-}
+} 
