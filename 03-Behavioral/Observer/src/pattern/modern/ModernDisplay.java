f02548b62d80f65551677691e2a372783747fbd2
Serge <sajghaa@gmail.com>
2026-09-12 20:58:36 +0200

    Updated ModernDisplay.java

--- 03-Behavioral/Observer/src/pattern/modern/ModernDisplay.java
+++ 03-Behavioral/Observer/src/pattern/modern/ModernDisplay.java
@@ -1,5 +1,27 @@
 package pattern.modern;
 
-public class ModernDisplay {
+import java.beans.PropertyChangeListener;
+
+public class ModernDisplay implements PropertyChangeListener {
+
+    private double temperature;
+    private double humidity;
+
+
+    @Override
+    public void propertyChange(PropertyChangeListener evt) {
+        String propertyName = evt.getPropertyName();
+        Object newValue = evt.getNewValue();
+
+        if ("temperature".equals(propertyName)) {
+            this.temperature = (double) newValue;
+        } else if ("humidity".equals(propertyName)) {
+            this.humidity = (double) newValue;
+        }
+
+        if (this.temperature != 0 && this.humidity != 0) {
+            System.out.println("Modern Display: Temp=" + temperature + "°C, Humidity=" + humidity + "%");
+        }
+    }
     
 }
