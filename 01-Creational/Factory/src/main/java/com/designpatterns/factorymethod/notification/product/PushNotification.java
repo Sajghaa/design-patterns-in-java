fc0f46dc226161c224391988ca5ed418452ee4a8
Serge <sajghaa@gmail.com>
2026-09-15 21:06:56 +0200

    Update PushNotification.java

--- 01-Creational/Factory/src/main/java/com/designpatterns/factorymethod/notification/product/PushNotification.java
+++ 01-Creational/Factory/src/main/java/com/designpatterns/factorymethod/notification/product/PushNotification.java
@@ -1,5 +1,30 @@
 package main.java.com.designpatterns.factorymethod.notification.product;
 
-public class PushNotification {
+import java.util.Objects;
+
+public final class PushNotification  implements Notification {
+
+    private  String recipient;
+    private String body;
+
+    @Override 
+    public void setRecipient(String recipient) {
+        this.recipient = Objects.requireNonNull(recipient, "recipient");
+    }
+
+    @Override
+    public void setBody(String body){
+        this.body = Objects.requireNonNull(body, "body");
+    }
+
+    @Override 
+    public void send() {
+        System.out.println(" -> Sending PUSH to device: " + recipient);
+        System.out.println("    → Payload: { \"title\": \"Notification\", \"body\": \"" + body + "\" }");
+    }
     
+    @Override
+    public String channel() {
+        return "PUSH";
+    }
 }
