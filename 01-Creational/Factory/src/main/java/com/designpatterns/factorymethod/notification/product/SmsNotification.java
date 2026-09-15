21dab3cad047057d5478258df6d8bca2d8aa7f92
Serge <sajghaa@gmail.com>
2026-09-15 20:57:36 +0200

    Update SmsNotification.java

--- 01-Creational/Factory/src/main/java/com/designpatterns/factorymethod/notification/product/SmsNotification.java
+++ 01-Creational/Factory/src/main/java/com/designpatterns/factorymethod/notification/product/SmsNotification.java
@@ -1,5 +1,32 @@
 package main.java.com.designpatterns.factorymethod.notification.product;
 
-public class SmsNotification {
+import java.util.Objects;
+
+public final class SmsNotification implements Notification{
     
+    private String recipient;
+    private String body;
+
+    @Override 
+    public void setRecipient(String recipient) {
+        this.recipient = Objects.requireNonNull(recipient, "recipient");
+    }
+
+    @Override 
+    public void setBody(String body) {
+        Objects.requireNonNull(body, "body");
+
+        this.body = body.length() > 160 ? body.substring(0, 157) + "..." : body;
+    }
+
+    @Override 
+    public void send() {
+        System.out.println(" -> sending SMS to " + recipient);
+        System.out.println(" -> Message: " + body);
+    }
+
+    @Override 
+    public  String channel() {
+        return "SMS";
+    }
 }
