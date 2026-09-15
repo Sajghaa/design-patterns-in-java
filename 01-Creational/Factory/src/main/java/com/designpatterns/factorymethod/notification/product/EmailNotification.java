58b649d78f68e59822212426bf68bd5127673599
Serge <sajghaa@gmail.com>
2026-09-15 20:56:26 +0200

    Adds EmailNotification.java

--- 01-Creational/Factory/src/main/java/com/designpatterns/factorymethod/notification/product/EmailNotification.java
+++ 01-Creational/Factory/src/main/java/com/designpatterns/factorymethod/notification/product/EmailNotification.java
@@ -1,5 +1,31 @@
 package main.java.com.designpatterns.factorymethod.notification.product;
 
-public class EmailNotification {
-    
+import java.util.Objects;
+
+public final class EmailNotification implements Notification {
+
+    private String recipient;
+    private String body;
+
+    @Override 
+    public void setRecipient(String recipient) {
+        this.recipient = Objects.requireNonNull(recipient, "recipient");
+    }
+
+    @Override
+    public void setBody(String body){
+        this.body = Objects.requireNonNull(body, "body");
+    }
+
+    @Override
+    public void send() {
+        System.out.println(" -> Sending EMAIL to " + recipient);
+        System.out.println(" -> Subject: Notification");
+        System.out.println(" ->Body: " + body);
+    }
+     
+    @Override
+    public  String channel(){
+        return "EMAIL";
+    }
 }
